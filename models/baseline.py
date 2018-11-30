ef93db43305a1ee9bb07990667c7c4b4ecdee365
Laura Manduchi <lauraman@student.ethz.ch>
2018-11-30 17:37:40 +0100

    add config

--- models/baseline.py
+++ models/baseline.py
@@ -16,43 +16,32 @@ class Baseline(BaseModel):
         self.x = tf.placeholder(tf.float32, shape=[None, 80, 80])
         self.y = tf.placeholder(tf.float32, shape=[None])
 
-        #param
-        filter_size_conv1 = 3
-        num_filters_conv1 = 32
-
-        filter_size_conv2 = 3
-        num_filters_conv2 = 32
-
-        filter_size_conv3 = 3
-        num_filters_conv3 = 64
-
-        fc_layer_size = 128
 
         # network architecture
         layer_conv1 = self.create_convolutional_layer(input=self.x,
-                                                 num_input_channels=num_channels,
-                                                 conv_filter_size=filter_size_conv1,
-                                                 num_filters=num_filters_conv1)
+                                                 num_input_channels=config.num_channels,
+                                                 conv_filter_size=config.filter_size_conv1,
+                                                 num_filters=config.num_filters_conv1)
 
         layer_conv2 = create_convolutional_layer(input=layer_conv1,
-                                                 num_input_channels=num_filters_conv1,
-                                                 conv_filter_size=filter_size_conv2,
-                                                 num_filters=num_filters_conv2)
+                                                 num_input_channels=config.num_filters_conv1,
+                                                 conv_filter_size=config.filter_size_conv2,
+                                                 num_filters=config.num_filters_conv2)
 
         layer_conv3 = create_convolutional_layer(input=layer_conv2,
-                                                 num_input_channels=num_filters_conv2,
-                                                 conv_filter_size=filter_size_conv3,
-                                                 num_filters=num_filters_conv3)
+                                                 num_input_channels=config.num_filters_conv2,
+                                                 conv_filter_size=config.filter_size_conv3,
+                                                 num_filters=config.num_filters_conv3)
 
         layer_flat = create_flatten_layer(layer_conv3)
 
         layer_fc1 = create_fc_layer(input=layer_flat,
                                     num_inputs=layer_flat.get_shape()[1:4].num_elements(),
-                                    num_outputs=fc_layer_size,
+                                    num_outputs=config.fc_layer_size,
                                     use_relu=True)
 
         layer_fc2 = create_fc_layer(input=layer_fc1,
-                                    num_inputs=fc_layer_size,
+                                    num_inputs=config.fc_layer_size,
                                     num_outputs=2,
                                     use_relu=False)
 
