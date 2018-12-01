e3f7c443416f4a99f597a2ee1567451df5b7f57d
Laura Manduchi <lauraman@student.ethz.ch>
2018-12-01 11:12:15 +0100

    Added try and except for loading data

--- data_loader/baseline_generator.py
+++ data_loader/baseline_generator.py
@@ -8,23 +8,15 @@ from data_loader.load_utils import load_obj
 class DataGenerator:
     def __init__(self, config):
         self.config = config
-        a = load_obj('../data/patches')
-        b = load_obj('../data/labels_patches')
-        self.input = a
-        self.y = b
-        
+
         # load data here
-        # Upload history of filters used from traffic_cat
+        try:
+            self.input = load_obj('../data/patches')
+            self.y = load_obj('../data/labels_patches')
 
-        # try:
-        #     self.input = load_obj('../data/patches')
-        #     self.y = load_obj('../data/labels_patches')
-
-        # Download history from traffic_cat
-
-        # except:
-        #     print("There is no data. Save it first (run data_loader/preprocessing.py)!! ")
-        #     exit(0)
+        except:
+            print("There is no data. Save it first (run data_loader/preprocessing.py)!! ")
+            exit(0)
 
     def next_batch(self, batch_size):
         idx = np.random.choice(len(self.input), batch_size)
